aca09850c232b7d72f0f2a08eaf5430d5927c96d
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-05-13 01:10:18 +0700

    qua met

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  const correctNumber = 54;
+
+  var correctNumber = Math.floor(Math.random() * 10);
+
   let turnLeft = 3;
   let history = [];
   document.getElementById("turnLeftArea").innerHTML = turnLeft;
@@ -11,16 +13,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("historyArea").innerHTML = `History:${history}`;
     document.getElementById("turnLeftArea").innerHTML = turnLeft;
     document.getElementById("notiArea").innerHTML = "";
+    document.getElementById("btn").disabled = false
+    document.getElementById("input").value = ""
+    correctNumber = Math.floor(Math.random() * 10);
   })
+  console.log("the correct number is :" + correctNumber)
+
+
+
 
 
   document.getElementById("btn").addEventListener("click", () => {
     var input = document.getElementById("input").value;
+    document.getElementById("resultArea").innerHTML = "";
     if (input) {
       history.find(elm => elm === input)
         ? alert("Duplicated input")
         : history.push(input);
-      if (turnLeft > 1 ) {
+      if (turnLeft > 0) {
         if (input > correctNumber) {
           document.getElementById("notiArea").innerHTML = `the number is too big`;
         } else if (input < correctNumber) {
@@ -30,22 +40,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
         } else {
           alert("correct ^^");
           turnLeft = 4;
-          console.log(turnLeft)
           history = [];
           document.getElementById("historyArea").innerHTML = `History:${history}`;
           document.getElementById("turnLeftArea").innerHTML = turnLeft;
           document.getElementById("notiArea").innerHTML = "";
+          document.getElementById("input").value = ""
+          correctNumber = Math.floor(Math.random() * 10);
         }
         turnLeft--;
         document.getElementById("input").value = "";
-      } else  {
-        document.getElementById("btn").disabled=true
+      } else {
+        document.getElementById("btn").disabled = true
         alert("Out of turn");
-        turnLeft=0;
-        history = [];
-        
-        
-        
+        turnLeft = 0;
+        document.getElementById("resultArea").innerHTML = `The result is:${correctNumber}`;
+         correctNumber = Math.floor(Math.random() * 10);
+
+
+
+
       }
     } else {
       alert("pls enter input");
