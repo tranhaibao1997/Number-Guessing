647169c9fb27bb7b832212d605152f6614702ead
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-05-13 01:45:44 +0700

    fixed

--- main.js
+++ main.js
@@ -20,29 +20,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log("the correct number is :" + correctNumber)
     document.getElementById("resultArea").innerHTML = "";
   })
-  
+
 
 
 
 
 
   document.getElementById("btn").addEventListener("click", () => {
+    console.log(turnLeft)
     var input = document.getElementById("input").value;
     document.getElementById("resultArea").innerHTML = "";
     if (input) {
-
-      if(history.find(elm => elm === input))
-      {
+      if (isNaN(input) === true) {
+        alert("Please enter number");
+        return;
+      }
+      if (history.find(elm => elm === input)) {
         alert("Duplicated input");
         return;
       }
-      else
-      {
+      else {
         history.push(input);
       }
-        // ? alert("Duplicated input");break;
-        // : 
-      if (turnLeft > 0) {
+      if (turnLeft > 1) {
         if (input > correctNumber) {
           document.getElementById("notiArea").innerHTML = `the number is too big`;
         } else if (input < correctNumber) {
@@ -63,12 +63,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         turnLeft--;
         document.getElementById("input").value = "";
       } else {
-        document.getElementById("btn").disabled = true
-        alert("Out of turn");
-        turnLeft = 0;
-        document.getElementById("resultArea").innerHTML = `The result is:${correctNumber}`;
-         correctNumber = Math.floor(Math.random() * 10);
-         console.log("the correct number is :" + correctNumber)
+        console.log(input,correctNumber)
+        if (input ==correctNumber) {
+      
+          alert("correct ^^");
+          turnLeft = 4;
+          history = [];
+          document.getElementById("historyArea").innerHTML = `History:${history}`;
+          document.getElementById("turnLeftArea").innerHTML = turnLeft;
+          document.getElementById("notiArea").innerHTML = "";
+          document.getElementById("input").value = ""
+          correctNumber = Math.floor(Math.random() * 10);
+          console.log("the correct number is :" + correctNumber)
+        }
+        else {
+          document.getElementById("btn").disabled = true
+          alert("Out of turn");
+          turnLeft = 0;
+          document.getElementById("resultArea").innerHTML = `The result is:${correctNumber}`;
+          correctNumber = Math.floor(Math.random() * 10);
+          console.log("the correct number is :" + correctNumber)
+        }
+
 
 
 
