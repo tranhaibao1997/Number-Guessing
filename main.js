e361c8f0d0b9d37d0df6582503fbacbcee2946b1
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-05-13 00:11:44 +0700

    fix turn

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
   let turnLeft = 3;
   let history = [];
   document.getElementById("turnLeftArea").innerHTML = turnLeft;
-  
-  document.getElementById("btnRS").addEventListener("click",()=>{
-     turnLeft = 3;
-     history = [];
-     document.getElementById("historyArea").innerHTML = `History:${history}`;
+
+  document.getElementById("btnRS").addEventListener("click", () => {
+    turnLeft = 3;
+    history = [];
+    document.getElementById("historyArea").innerHTML = `History:${history}`;
     document.getElementById("turnLeftArea").innerHTML = turnLeft;
     document.getElementById("notiArea").innerHTML = "";
   })
-  
-  
+
+
   document.getElementById("btn").addEventListener("click", () => {
     var input = document.getElementById("input").value;
     if (input) {
       history.find(elm => elm === input)
         ? alert("Duplicated input")
         : history.push(input);
-      if (turnLeft > 0) {
+      if (turnLeft > 1 ) {
         if (input > correctNumber) {
           document.getElementById("notiArea").innerHTML = `the number is too big`;
         } else if (input < correctNumber) {
@@ -29,17 +29,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
           ).innerHTML = `the number is too small`;
         } else {
           alert("correct ^^");
-          turnLeft = 3;
+          turnLeft = 4;
+          console.log(turnLeft)
           history = [];
           document.getElementById("historyArea").innerHTML = `History:${history}`;
-         document.getElementById("turnLeftArea").innerHTML = turnLeft;
-         document.getElementById("notiArea").innerHTML = "";
+          document.getElementById("turnLeftArea").innerHTML = turnLeft;
+          document.getElementById("notiArea").innerHTML = "";
         }
         turnLeft--;
         document.getElementById("input").value = "";
-      } else {
+      } else  {
+        document.getElementById("btn").disabled=true
         alert("Out of turn");
+        turnLeft=0;
         history = [];
+        
+        
+        
       }
     } else {
       alert("pls enter input");
@@ -47,5 +53,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
     document.getElementById("historyArea").innerHTML = `History:${history}`;
     document.getElementById("turnLeftArea").innerHTML = turnLeft;
-  });  
+  });
 });
